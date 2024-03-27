ef66a2315c1700419a172a000781c961528d1862
Keroles-Nadyy <keroles_N@outlook.com>
2024-03-27 18:18:28 +0200

    adding search view and search_results.html

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from homepage.views import landing
+from homepage.views import landing, search
 
 
 urlpatterns = [
     path('', landing, name="home.landing"),
+    path('search/', search, name="search"),
 ]
