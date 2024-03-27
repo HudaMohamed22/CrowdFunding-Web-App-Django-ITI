c76941f682f856f90d04e4c0bfd8254a0edd2f4c
Keroles-Nadyy <keroles_N@outlook.com>
2024-03-27 18:07:37 +0200

    Revert "adding search view and result.html"
    This reverts commit 3efdede21885cb992514eb9ea0b3e81bddaabd00.

--- homepage/views.py
+++ homepage/views.py
@@ -1,29 +1,6 @@
 from django.shortcuts import render
-from homepage.forms import SearchForm
-from project.models import Project, Tag
 
 # Create your views here.
 
 def landing(request):
     return render(request, "homepage/homepage.html")
-
-
-def search(request):
-    form = SearchForm(request.GET)
-    if form.is_valid():
-        search_option = form.cleaned_data['search_option']
-        query = form.cleaned_data['query']
-        
-        if search_option == 'project':
-            results = Project.objects.filter(title__icontains=query)
-        elif search_option == 'tag':
-            try:
-                tag = Tag.objects.get(name__iexact=query)
-                results = tag.projects.all()
-            except:
-                results = []
-        else:
-            results = []
-    else:
-        results = []
-    return render(request, 'homepage/search_results.html', {'searchForm': form, 'searchResults': results})
